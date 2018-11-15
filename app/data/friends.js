6dbd68fbb1a477c75a240f823d60f045691c66e4
Valverde Daniel <valverdedaniel44@gmail.com>
2018-11-15 15:10:00 -0500

    initial bulk commit

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+// Variable to hold the array of friends objects data
+var friends = [
+    {
+        "name": "this guy",
+        "photo": "https://files.cults3d.com/uploaders/7302754/illustration-file/15a62693-ef9f-438c-aa3a-392a1f7c82cb/present%20jedi%20pinshape_large.jpg",
+        "scores": [
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1"
+        ]
+        }, 
+{
+    "name": "bart simpson",
+    "photo": "https://static.giantbomb.com/uploads/square_medium/4/48992/1089078-bart.png",
+    "scores": [
+    "5",
+    "5",
+    "5",
+    "5",
+    "5",
+    "5",
+    "5",
+    "5",
+    "5",
+    "5"
+    ]
+    }];
+
+// Export the data
+module.exports = friends;
